002e6879839575ff45a8b94fb6b0fc19c40423de
Mark Beale <bealebeaverst@gmail.com>
2024-07-15 13:41:00 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(startingLocation){
+  const dist = startingLocation
+  if (dist <= 400){
+    return "This one is on me!";
+  }
+  if (dist <= 2000){
+    return "That will be twenty bucks.";
+  }
+  if (dist <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  if (dist > 2500){
+    return "No can do.";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(custResp){
+  switch(custResp){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+
+  }
   // Write your code here!
 }
